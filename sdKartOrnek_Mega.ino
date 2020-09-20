eda573c4c008ff6ae937453ff216fccc901212cc
AltayAtaman <71609322+AltayAtaman@users.noreply.github.com>
2020-09-20 23:31:02 +0300

    Add files via upload

---
+++ sdKartOrnek_Mega.ino
@@ -0,0 +1,30 @@
+#include <SD.h>
+
+File dosya;
+
+const byte sdCS = 49;
+
+void setup() 
+{
+  Serial.begin(9600);
+
+  pinMode(sdCS, OUTPUT);
+
+  if (!SD.begin(sdCS)) 
+    Serial.println("Mikro SD kart modulu basirili bir sekilde baslatildi!");
+
+  else
+  {
+    Serial.println("Mikro SD kart modulu baslatilirken bir problem olustu!");
+    while(1);
+  }
+}
+
+void loop() 
+{
+  dosya = SD.open("sdTest.txt", FILE_WRITE);
+  dosya.println("Zaman: " + String(millis() / 1000.0));
+
+  dosya.close();
+  delay(750);
+}
